7d991b2918c56671ad982288b317d5a1bc039aa6
Admin <admin@adminde-mac-pro.local>
2018-08-14 15:49:22 +0800

    add Car class

--- iOSClub/ViewController.swift
+++ iOSClub/ViewController.swift
@@ -9,9 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var type: UILabel!
+    @IBOutlet weak var brand: UILabel!
+    @IBOutlet weak var plate: UITextField!
+    @IBOutlet weak var consume: UILabel!
+    @IBOutlet weak var passengers: UILabel!
+    @IBOutlet weak var goods: UILabel!
+    @IBOutlet weak var stock: UILabel!
+    @IBOutlet weak var miles: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    
+    var cars = [Car(brand: "Honda", plate: "AP-123"),
+                Car(brand: "Toyota", plate: "GG-3B0"),
+                Car(brand: "Mazda", plate: "MM-204"),
+                Car(brand: "Ford", plate: "QQ-340"),
+                Car(brand: "Lexus", plate: "RR-074"),]
+    
+    var choose = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        slider.minimumValue = 0
+        slider.maximumValue = Float(cars.count - 1)
+        slider.value = 0
+        refresh()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +40,58 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func refresh(){
+        type.text = "汽車"
+        brand.text = cars[choose].getBrand()
+        plate.text = cars[choose].getPlate()
+        consume.text = String(cars[choose].getConsume())
+        passengers.text = String(cars[choose].getPassengers())
+        goods.text = String(cars[choose].getGoods())
+        stock.text = String(cars[choose].getStock())
+        miles.text = String(cars[choose].getMiles())
+    }
 
-
+    @IBAction func changePlate(_ sender: Any) {
+        cars[choose].setPlate(plate: plate.text!)
+        refresh()
+    }
+    
+    @IBAction func addPassenger(_ sender: Any) {
+        cars[choose].addPassenger()
+        refresh()
+    }
+    
+    @IBAction func removePassenger(_ sender: Any) {
+        cars[choose].removePassenger()
+        refresh()
+    }
+    
+    @IBAction func addGood(_ sender: Any) {
+        cars[choose].addGood()
+        refresh()
+    }
+    
+    @IBAction func removeGood(_ sender: Any) {
+        cars[choose].removeGood()
+        refresh()
+    }
+    
+    @IBAction func drive(_ sender: Any) {
+        cars[choose].drive()
+        refresh()
+    }
+    
+    @IBAction func refuel(_ sender: Any) {
+        cars[choose].refuel()
+        refresh()
+    }
+    
+    @IBAction func selected(_ sender: Any) {
+        slider.value = Float(Int(slider.value))
+        choose = Int(slider.value)
+        refresh()
+    }
+    
 }
 
